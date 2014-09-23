a6763513848f312e5aff8628de3046e9bc8956a2
giovanyhernandez <giovany9216@gmail.com>
2014-09-23 19:33:52 +0200

    haciendo

--- src/Circulo.java
+++ src/Circulo.java
@@ -1,1 +1,25 @@
-class
+import java.util.Scanner;
+
+public class Circulo {
+
+public static void Circulo (String args []) {
+
+	double radio, area; 
+	Scanner sc = new Scanner(System.in);
+	System.out.print("\ningresa el radio: ");
+	radio = sc.nexFloat();
+
+
+ 	resultadodelradio = OperacionesCirculo.area(area);
+ 	resultadodelacircunf = OperacionesCirculo.circunf(radio);
+
+
+	System.out.println("\nel radio del circulo es: " + resultadodelradio );
+	System.out.println("\nel resultado de la circunferencia es " + resultadodelacircunf);
+	
+}
+}	 
+
+
+
+ 
--- src/OperacionesCirculo.java
+++ src/OperacionesCirculo.java
@@ -1,1 +1,19 @@
-class
+public class OperacionesCirculo { 
+
+
+public double area(radio) { //encontrar el radio.
+
+    double result = 0;
+    result = (3.1416*radio*radio);
+    return result;
+
+}
+
+public double circunf(circunf) { //encontrar la circunferencia
+
+    double result= 0;
+    result = (2*3.1416*radio);
+    return result; 
+
+}
+}
